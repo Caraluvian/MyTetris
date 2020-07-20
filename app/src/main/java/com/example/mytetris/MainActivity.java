3db0f773e8542dd56f25a3d62040a2e0da170310
Cara Li <47161480+Caraluvian@users.noreply.github.com>
2020-07-20 01:28:17 -0400

    Finish drawing the maps and boxes of the game screen

--- app/src/main/java/com/example/mytetris/MainActivity.java
+++ app/src/main/java/com/example/mytetris/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //hide the action bar on the top
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
